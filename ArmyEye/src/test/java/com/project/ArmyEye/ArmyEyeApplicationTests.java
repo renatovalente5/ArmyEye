0a2ec7ba0f7a2bb76c2c90ee5807f6d21d965980
Dário Matos <dario.matos@ua.pt>
2021-05-27 16:08:44 +0100

    Adição de Testes Maven

--- ArmyEye/src/test/java/com/project/ArmyEye/ArmyEyeApplicationTests.java
+++ ArmyEye/src/test/java/com/project/ArmyEye/ArmyEyeApplicationTests.java
@@ -5,6 +5,7 @@ import com.project.ArmyEye.Models.GPS;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -24,8 +25,67 @@ class ArmyEyeApplicationTests {
 //	}
 
 	@Test
-	void checkFile(){
+	void fileGPSExists() {
+
+		File tmp = new File("src/main/java/com/project/ArmyEye/sample_data/GPS.tsv");
+		assert (tmp.exists());
+	}
+
+	@Test
+	void fileGPSValid(){
 		List<String[]> l=ArmyEyeController.tsvr("src/main/java/com/project/ArmyEye/sample_data/GPS.tsv");
 			assert(l.size()!=0);
+	}
+
+	@Test
+	void fileHelmetExists() {
+
+		File tmp = new File("src/main/java/com/project/ArmyEye/sample_data/Helmet.tsv");
+		assert (tmp.exists());
+	}
+
+	@Test
+	void fileHelmetValid(){
+		List<String[]> l=ArmyEyeController.tsvr("src/main/java/com/project/ArmyEye/sample_data/Helmet.tsv");
+		assert(l.size()!=0);
+	}
+
+	@Test
+	void fileBatteryExists() {
+
+		File tmp = new File("src/main/java/com/project/ArmyEye/sample_data/Unit_Battery.tsv");
+		assert (tmp.exists());
+	}
+
+	@Test
+	void fileBatteryValid(){
+		List<String[]> l=ArmyEyeController.tsvr("src/main/java/com/project/ArmyEye/sample_data/Unit_Battery.tsv");
+		assert(l.size()!=0);
+	}
+
+	@Test
+	void fileVitalECGExists() {
+
+		File tmp = new File("src/main/java/com/project/ArmyEye/sample_data/VitalJacket_ECG.tsv");
+		assert (tmp.exists());
+	}
+
+	@Test
+	void fileVitalECGValid(){
+		List<String[]> l=ArmyEyeController.tsvr("src/main/java/com/project/ArmyEye/sample_data/VitalJacket_ECG.tsv");
+		assert(l.size()!=0);
+	}
+
+	@Test
+	void fileVitalOthersExists() {
+
+		File tmp = new File("src/main/java/com/project/ArmyEye/sample_data/VitalJacket_others.tsv");
+		assert (tmp.exists());
+	}
+
+	@Test
+	void fileVitalOthersValid(){
+		List<String[]> l=ArmyEyeController.tsvr("src/main/java/com/project/ArmyEye/sample_data/GPS.tsv");
+		assert(l.size()!=0);
 	}
 }
