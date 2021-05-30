e09d1f720b5c0f2e028a7b7882915ac8378f603b
Dário Matos <dario.matos@ua.pt>
2021-05-30 20:33:06 +0100

    Deploy feito e BD na VM

--- ArmyEye/src/main/java/com/project/ArmyEye/Controllers/ArmyEyeController.java
+++ ArmyEye/src/main/java/com/project/ArmyEye/Controllers/ArmyEyeController.java
@@ -48,7 +48,7 @@ public class ArmyEyeController {
             int i = 0;
             for (String[] str : armys) {
                 if (i > 0 && i < 100) {
-                    //gpsRepository.save(new GPS(str[0], str[1], str[2], str[3], str[4], str[5]));
+                    gpsRepository.save(new GPS(str[0], str[1], str[2], str[3], str[4], str[5]));
                     armyGPS.add(new GPS(str[0], str[1], str[2], str[3], str[4], str[5]));
                     System.out.println(str[0] + " " + str[1] + " " + str[2] + " " + str[3] + " " + str[4] + " " + str[5]);
                 }
