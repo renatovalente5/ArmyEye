75f7e6308f63f28772f38038ea9c7d2efab3e913
Dário Matos <dario.matos@ua.pt>
2021-06-05 15:04:14 +0100

    Tentativa

--- ArmyEye/src/main/java/com/project/ArmyEye/Controllers/ArmyEyeController.java
+++ ArmyEye/src/main/java/com/project/ArmyEye/Controllers/ArmyEyeController.java
@@ -42,7 +42,7 @@ public class ArmyEyeController {
     private final HelmetRepository getHelmetRepository;
     private final ECGRepository getECGRepository;
 
-    private boolean init = true;
+    private boolean init = false;
     private int count = 0;
     private static LinkedList<GPS> armyGPS;
     private static LinkedList<Helmet> armyHelmet;
@@ -99,6 +99,10 @@ public class ArmyEyeController {
             //init = false;
         }else {
 //GPS
+            armyGPS= (LinkedList<GPS>) gpsRepository.findAll();
+            armyHelmet= (LinkedList<Helmet>) getHelmetRepository.findAll();
+            armyECG= (LinkedList<VitalJacket_ECG>) getECGRepository.findAll();
+
             GPS auxGPS = armyGPSaux.getFirst();
             armyGPSaux.removeFirst();
             System.out.println("--Deu Passo----" + auxGPS.getAltitude());
